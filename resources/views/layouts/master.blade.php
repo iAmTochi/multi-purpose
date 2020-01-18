244783ffb5fe58f2a6bf801af1bf446d34bb9d4e
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-01-19 00:10:46 +0100

    Create User Managment Component

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -86,10 +86,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </a>
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
-                                <a href="#" class="nav-link ">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Active Page</p>
-                                </a>
+                                <router-link to="/users" class="nav-link ">
+                                    <i class="fas fa-users nav-icon"></i>
+                                    <p>Users</p>
+                                </router-link>
                             </li>
                             <li class="nav-item">
                                 <a href="#" class="nav-link">
