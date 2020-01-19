645ea7375bcde9c0ec7dba86610e42031d6ad9b3
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-01-19 23:07:10 +0100

    Security Issues While Developing API in Laravel and Vuejs

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,6 +9,18 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('api');
+    }
+
+
     /**
      * Display a listing of the resource.
      *
