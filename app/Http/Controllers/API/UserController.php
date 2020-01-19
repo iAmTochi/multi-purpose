f4dedbe2d9f5330f0144c0143c56bea5b200a1d9
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-01-19 02:13:25 +0100

    server validation

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -27,6 +27,11 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request,[
+            'name'=> 'required|string|max:191',
+            'email'=> 'required|email|string|max:191|unique:users',
+            'password'=> 'required|string|min:8',
+        ]);
         return User::create([
             'name'=> $request->name,
             'email'=> $request->email,
